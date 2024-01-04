c18e891c6cc838afe1a7146cb0eb0c77c6acd3ef
Hollybright <brightrobinson.official@gmail.com>
2024-01-04 12:44:51 +0100

    Importing simple function frol file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Value of the variables
+a = 1
+b = 2
+
+from add_0 import add #Import functions from add_0
+
+output = add(a, b) #Imported result to be calculated
+
+print("{} + {} = {}".format(a, b, output))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Readme files for python import modules projects, in ALX spring 2
