8048856621227214f4e572e89bad2b2f6aebe78b
Hollybright <brightrobinson.official@gmail.com>
2024-01-04 14:47:20 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
 
 #Value of the variables
 a = 1
@@ -9,4 +8,4 @@ from add_0 import add #Import functions from add_0
 
 output = add(a, b) #Imported result to be calculated
 
-print("{} + {} = {}".format(a, b, output))
+print(f"{a} + {b} = {add(a, b)}")
