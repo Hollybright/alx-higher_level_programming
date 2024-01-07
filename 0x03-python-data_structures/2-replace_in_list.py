65047552a7565be064c5c179557efc0b211d434e
Hollybright <brightrobinson.official@gmail.com>
2024-01-07 20:05:48 +0100

    replace number

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+
+def replace_in_list(my_list, idx, element):
+    my_list_len = len(my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
