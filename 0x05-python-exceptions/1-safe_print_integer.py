9ff5a14667fb259b341499b3843614f0e5d67f8f
Hollybright <brightrobinson.official@gmail.com>
2024-01-22 12:58:47 +0100

    print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-
-def safe_print_list_integers(my_list=[], x=0):
-
-    best = 0
-    for c in range(0, x):
-        try:
-
-            print("{:d}".format(my_list[c]), end="")
-            best += 1
-
-        except (ValueError, TypeError):
-            pass
-
-        print()
-        return best
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
