59a825cf593f781e60a6b62737b5e3268b451d84
Hollybright <brightrobinson.official@gmail.com>
2024-01-23 08:18:49 +0100

    print safely list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
-    best = 0
-    for x in range(0, x):
+    bet = 0
+    for i in range(0, x):
         try:
-
-            print("{:d}".format(my_list[x]), end="")
-            best += 1
-
+            print("{:d}".format(my_list[i]), end="")
+            bet += 1
         except (ValueError, TypeError):
             pass
 
         print()
-        return best
+        return bet
