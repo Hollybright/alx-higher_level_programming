072e7a3a9b5809354778fc833774dc852e3822f0
Hollybright <brightrobinson.official@gmail.com>
2024-01-23 23:14:05 +0100

    Square size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-    # Represents a square.
-    # Private instance attribute: size.
-    # Instantiation with size (no type/value verification).
-    
+    """ Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
 
     def __init__(self, size):
-        # Initializes the data.
+        """ Initializes the data. """
         self.__size = size
